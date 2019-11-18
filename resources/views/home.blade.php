78b5226889dba577692b37b9c46aecbe3383c2cd
Hossein <me@ahmadpanah.net>
2019-11-18 20:51:57 +0330

    First Commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="/varzesh3.jpeg" class="rounded-circle" style="width:170px";>
+        </div>
+    <div class="col-9 p-5" >
+            <div><h1>varzesh3</h1></div>
+            <div class="d-flex">
+                <div class="pr-3"> <strong>12,896</strong> posts </div>
+                <div class="pr-3"> <strong>2m</strong> followers</div>
+                <div class="pr-3"> <strong>9</strong> following</div>
+            </div>
+            <div class="pt-4">Varzesh3 ورزش‌سه </div>
+            <div>ورزش سه</div>
+            <div><a href='#'>www.varzesh3.com</a></div>
+    </div>
+    </div>
+
+    <div class="row pt-4">
+        <div class="col-4">
+            <img src="https://instagram.fevn1-3.fna.fbcdn.net/vp/47f545b04766e567aacb1f96d13dd829/5E8A3E6E/t51.2885-15/e35/c0.135.1080.1080a/s150x150/72349569_147843469932731_5471819444436288990_n.jpg?_nc_ht=instagram.fevn1-3.fna.fbcdn.net&_nc_cat=111 150w,https://instagram.fevn1-3.fna.fbcdn.net/vp/b1121e94ecb0c8489ea1fa81a081071d/5E65ED24/t51.2885-15/e35/c0.135.1080.1080a/s240x240/72349569_147843469932731_5471819444436288990_n.jpg?_nc_ht=instagram.fevn1-3.fna.fbcdn.net&_nc_cat=111 240w,https://instagram.fevn1-3.fna.fbcdn.net/vp/e32c8e069597a8e9954bff525ef84d08/5E4E079E/t51.2885-15/e35/c0.135.1080.1080a/s320x320/72349569_147843469932731_5471819444436288990_n.jpg?_nc_ht=instagram.fevn1-3.fna.fbcdn.net&_nc_cat=111 320w,https://instagram.fevn1-3.fna.fbcdn.net/vp/5e325831f13ec712e248a701231cc6ed/5E4E37C4/t51.2885-15/e35/c0.135.1080.1080a/s480x480/72349569_147843469932731_5471819444436288990_n.jpg?_nc_ht=instagram.fevn1-3.fna.fbcdn.net&_nc_cat=111 480w,https://instagram.fevn1-3.fna.fbcdn.net/vp/a9c6916ac0acfd88ec7670ac2a74a76c/5E567AA5/t51.2885-15/sh0.08/e35/c0.135.1080.1080a/s640x640/72349569_147843469932731_5471819444436288990_n.jpg?_nc_ht=instagram.fevn1-3.fna.fbcdn.net&_nc_cat=111 640w" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://instagram.fevn1-3.fna.fbcdn.net/vp/47f545b04766e567aacb1f96d13dd829/5E8A3E6E/t51.2885-15/e35/c0.135.1080.1080a/s150x150/72349569_147843469932731_5471819444436288990_n.jpg?_nc_ht=instagram.fevn1-3.fna.fbcdn.net&_nc_cat=111 150w,https://instagram.fevn1-3.fna.fbcdn.net/vp/b1121e94ecb0c8489ea1fa81a081071d/5E65ED24/t51.2885-15/e35/c0.135.1080.1080a/s240x240/72349569_147843469932731_5471819444436288990_n.jpg?_nc_ht=instagram.fevn1-3.fna.fbcdn.net&_nc_cat=111 240w,https://instagram.fevn1-3.fna.fbcdn.net/vp/e32c8e069597a8e9954bff525ef84d08/5E4E079E/t51.2885-15/e35/c0.135.1080.1080a/s320x320/72349569_147843469932731_5471819444436288990_n.jpg?_nc_ht=instagram.fevn1-3.fna.fbcdn.net&_nc_cat=111 320w,https://instagram.fevn1-3.fna.fbcdn.net/vp/5e325831f13ec712e248a701231cc6ed/5E4E37C4/t51.2885-15/e35/c0.135.1080.1080a/s480x480/72349569_147843469932731_5471819444436288990_n.jpg?_nc_ht=instagram.fevn1-3.fna.fbcdn.net&_nc_cat=111 480w,https://instagram.fevn1-3.fna.fbcdn.net/vp/a9c6916ac0acfd88ec7670ac2a74a76c/5E567AA5/t51.2885-15/sh0.08/e35/c0.135.1080.1080a/s640x640/72349569_147843469932731_5471819444436288990_n.jpg?_nc_ht=instagram.fevn1-3.fna.fbcdn.net&_nc_cat=111 640w" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://instagram.fevn1-3.fna.fbcdn.net/vp/47f545b04766e567aacb1f96d13dd829/5E8A3E6E/t51.2885-15/e35/c0.135.1080.1080a/s150x150/72349569_147843469932731_5471819444436288990_n.jpg?_nc_ht=instagram.fevn1-3.fna.fbcdn.net&_nc_cat=111 150w,https://instagram.fevn1-3.fna.fbcdn.net/vp/b1121e94ecb0c8489ea1fa81a081071d/5E65ED24/t51.2885-15/e35/c0.135.1080.1080a/s240x240/72349569_147843469932731_5471819444436288990_n.jpg?_nc_ht=instagram.fevn1-3.fna.fbcdn.net&_nc_cat=111 240w,https://instagram.fevn1-3.fna.fbcdn.net/vp/e32c8e069597a8e9954bff525ef84d08/5E4E079E/t51.2885-15/e35/c0.135.1080.1080a/s320x320/72349569_147843469932731_5471819444436288990_n.jpg?_nc_ht=instagram.fevn1-3.fna.fbcdn.net&_nc_cat=111 320w,https://instagram.fevn1-3.fna.fbcdn.net/vp/5e325831f13ec712e248a701231cc6ed/5E4E37C4/t51.2885-15/e35/c0.135.1080.1080a/s480x480/72349569_147843469932731_5471819444436288990_n.jpg?_nc_ht=instagram.fevn1-3.fna.fbcdn.net&_nc_cat=111 480w,https://instagram.fevn1-3.fna.fbcdn.net/vp/a9c6916ac0acfd88ec7670ac2a74a76c/5E567AA5/t51.2885-15/sh0.08/e35/c0.135.1080.1080a/s640x640/72349569_147843469932731_5471819444436288990_n.jpg?_nc_ht=instagram.fevn1-3.fna.fbcdn.net&_nc_cat=111 640w" class="w-100">
+        </div>
+    </div>
+</div>
+@endsection
